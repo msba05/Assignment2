9dbeab55771dc9553e6a1678e417a3902472c1ab
mariam :D <mariam.sonnah@gmail.com>
2025-07-29 18:32:01 -0500

    Create GraduateStudent.java

---
+++ Graduate.java
@@ -0,0 +1,36 @@
+public class GraduateStudent extends Student
+{
+    private String major="";
+    private String thesisTopic="";
+
+    public GraduateStudent()
+    {
+        super();
+    }
+    public GraduateStudent(String n, String ID,double gpa,int credits,String major, String thesisTopic) throws InvalidStudentIDException, InvalidStudentNameException {
+        super(n,ID,gpa,credits);
+        this.major = major;
+        this.thesisTopic = thesisTopic;
+    }
+    public String getMajor()
+    {
+        return major;
+    }
+
+    public void setMajor(String major)
+    {
+        this.major = major;
+    }
+    public String getThesisTopic() {
+        return thesisTopic;
+    }
+
+    public void setThesisTopic(String thesisTopic) {
+        this.thesisTopic = thesisTopic;
+    }
+    @Override
+    public String toString()
+    {
+        return "Graduate Student: "+getName() +", ID: "+getStudentID()+", Major: "+getMajor()+", GPA: "+getGpa()+", Credits: "+getCredits()+", Thesis Topic: "+getThesisTopic();
+    }
+}
