5072f89c0ea5c32665e092842747e5e688c32ec1
Trevor <149530191+sandetre000@users.noreply.github.com>
2025-07-29 18:29:19 -0500

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,113 @@
+package studentperformancepredictor;
+
+public class LinkedList<T> {
+    private Node<T> head;
+
+    public void insertAtHead(T data) {
+        Node<T> newNode = new Node<>(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void insertAtTail(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node<T> current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public void insertAtPosition(T data, int index) {
+        if (index == 0) {
+            insertAtHead(data);
+            return;
+        }
+        Node<T> current = head;
+        for (int i = 0; i < index - 1 && current != null; i++) {
+            current = current.next;
+        }
+        if (current == null) return;
+
+        Node<T> newNode = new Node<>(data);
+        newNode.next = current.next;
+        current.next = newNode;
+    }
+
+    public void removeByValue(T data) {
+        if (head == null) return;
+        if (head.data.equals(data)) {
+            head = head.next;
+            return;
+        }
+        Node<T> current = head;
+        while (current.next != null && !current.next.data.equals(data)) {
+            current = current.next;
+        }
+        if (current.next != null) {
+            current.next = current.next.next;
+        }
+    }
+
+    public void removeAtIndex(int index) {
+        if (head == null) return;
+        if (index == 0) {
+            head = head.next;
+            return;
+        }
+        Node<T> current = head;
+        for (int i = 0; i < index - 1 && current.next != null; i++) {
+            current = current.next;
+        }
+        if (current.next != null) {
+            current.next = current.next.next;
+        }
+    }
+
+    public boolean search(T data) {
+        Node<T> current = head;
+        while (current != null) {
+            if (current.data.equals(data)) return true;
+            current = current.next;
+        }
+        return false;
+    }
+
+    public void display() {
+        Node<T> current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public int size() {
+        int count = 0;
+        Node<T> current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    public T[] toArray(T[] arrayTemplate) {
+        int size = size();
+        T[] array = java.util.Arrays.copyOf(arrayTemplate, size);
+        Node<T> current = head;
+        for (int i = 0; i < size; i++) {
+            array[i] = current.data;
+            current = current.next;
+        }
+        return array;
+    }
+}
+
