32e0a1b842fbecd1879132a65abaeb00aa39b80d
mariam :D <mariam.sonnah@gmail.com>
2025-07-30 21:30:42 -0500

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,4 +1,7 @@
-public class Node<T> {
+import java.io.Serializable;
+
+public class Node<T>  implements Serializable {
+    private static final long serialVersionUID =1L;
     T data;
     Node<T> next;
 
